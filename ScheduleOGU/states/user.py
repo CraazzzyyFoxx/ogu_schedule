6d94626beb2203468c54c76788f473f069ae012f
CraazzzyyFoxx <andrey.pod19@gmail.com>
2022-03-14 10:56:17 +0300

    Удалил неиспользуемый код

--- ScheduleOGU/states/user.py
+++ ScheduleOGU/states/user.py
@@ -1,15 +1,10 @@
 from aiogram.dispatcher.filters.state import StatesGroup, State
 
 
-__all__ = ("Register", "Test")
+__all__ = ("Register",)
 
 
 class Register(StatesGroup):
     faculty = State()
     course = State()
     group = State()
-
-
-class Test(StatesGroup):
-    Q1 = State()
-    Q2 = State()
